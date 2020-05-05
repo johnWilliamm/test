c20e5b55cf06beee8921225094517dd26e906ed7
iman-7 <iman.elsadany@aucegypt.edu>
2020-05-05 12:24:21 +0200

    Hey It's Me! Iman!

---
+++ headers/iman.h
@@ -0,0 +1,6 @@
+#ifndef iman_h
+#define iman_h
+#include <iostream>
+using namespace std;
+void iman();
+#endif
---
+++ sources/iman.cpp
@@ -0,0 +1,6 @@
+#include "iman.h"
+void iman(){
+cout << "I am Iman." << endl;
+cout << "My email is: iman.elsadany@aucegypt.edu" <<endl;
+}
+
